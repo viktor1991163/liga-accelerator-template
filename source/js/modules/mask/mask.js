43edcb96e456c414e940e8cb5fdfd4a3367b965c
viktor <ignatev-viktor@yandex.ru>
2022-11-11 20:04:11 +0800

    исправления багов после тестирования

--- source/js/modules/mask/mask.js
+++ source/js/modules/mask/mask.js
@@ -1,11 +1,20 @@
 const telInput = document.querySelectorAll('[type="tel"]')
 
   const maskPhone = () => {
-    telInput.forEach(e => {
+    telInput.forEach(element => {
+
+      element.oninput = function(event) {
+        event.target.setCustomValidity('');
+      };
+
+      element.oninvalid = function(event) {
+        event.target.setCustomValidity('Введите номер в верном формате');
+      };
+
       const maskOptions = {
         mask: '+{7}(000)000-00-00'
       };
-      const mask = IMask(e, maskOptions);
+      const mask = IMask(element, maskOptions);
     });
   };
 export {maskPhone};
